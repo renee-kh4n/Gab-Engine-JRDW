4941858ada6426dcc521e3e495caa1ca271d7d83
Cybalt-SR <cybalt.sr@gmail.com>
2024-01-16 16:10:19 +0800

    Update GDgrapProk.cpp

--- GDgrapProk/GDgrapProk.cpp
+++ GDgrapProk/GDgrapProk.cpp
@@ -1,5 +1,8 @@
 #include <GLFW/glfw3.h>
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 int main(void)
 {
     GLFWwindow* window;
@@ -9,14 +12,17 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    auto win_x = 700;
+    auto win_y = 400;
+    window = glfwCreateWindow(win_x, win_y, "Hello World", NULL, NULL);
     if (!window)
     {
-
-
         glfwTerminate();
         return -1;
     }
+    auto pixelPerfectGLV2F = [win_x, win_y](float x, float y) {
+        glVertex2f(x / win_x, y / win_y);
+        };
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -26,6 +32,24 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_POLYGON);
+        
+        auto vertexCount = 5;
+        auto shapeSize = (win_x < win_y) ? win_x : win_y;
+        auto offsetRad = M_PI * 0.5f;
+
+        for (size_t v_i = 0; v_i < vertexCount; v_i++)
+        {
+            auto t = (float)v_i / vertexCount;
+            auto rad = (t * M_PI * 2) + offsetRad;
+            auto v_x = cosf(rad) * shapeSize;
+            auto v_y = sinf(rad) * shapeSize;
+
+            pixelPerfectGLV2F(v_x, v_y);
+        }
+
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
