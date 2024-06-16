67a01de2d25e8eab522e01a1a34ff3d47e5a87a3
Cybalt-SR <cybalt.sr@gmail.com>
2024-06-16 22:34:03 +0800

    Update MouseRightDragImplementation.h

--- GDPHYSX/Dependencies/include/GD_Engine/Input/Implementations/MouseRightDragImplementation.h
+++ GDPHYSX/Dependencies/include/GD_Engine/Input/Implementations/MouseRightDragImplementation.h
@@ -5,7 +5,7 @@
 
 namespace gde {
 	namespace input {
-		class MouseRightDragImplementation : public ActionImplementation<MouseRightDrag> {
+		class MouseRightDragImplementation : public ActionImplementation<MouseRightDrag> {			
 			// Inherited via ActionImplementation
 			virtual void UpdateState() override;
 		};
