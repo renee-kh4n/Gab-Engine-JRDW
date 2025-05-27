98ef1eda019105da19791f65a4f4b46a757b11f4
Cybalt-SR <87524313+Cybalt-SR@users.noreply.github.com>
2025-05-27 21:05:14 +0800

    Update RenderPipeline.cpp

--- src/Graphics/RenderPipeline.cpp
+++ src/Graphics/RenderPipeline.cpp
@@ -855,7 +855,8 @@ void gbe::RenderPipeline::RenderFrame(Matrix4& viewmat, Matrix4& projmat, float&
                 VkDeviceSize offsets[] = { 0 };
                 vkCmdBindVertexBuffers(currentCommandBuffer, 0, 1, vertexBuffers, offsets);
 
-                vkCmdBindDescriptorSets(currentCommandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, currentshaderdata.pipelineLayout, 0, 1, &callinstance.allocdescriptorSets[currentFrame], 0, nullptr);
+                auto dallocset = &callinstance.allocdescriptorSets[currentFrame];
+                vkCmdBindDescriptorSets(currentCommandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, currentshaderdata.pipelineLayout, 0, 1, dallocset, 0, nullptr);
                 
                 vkCmdBindIndexBuffer(currentCommandBuffer, curmesh.indexBuffer, 0, VK_INDEX_TYPE_UINT16);
                 vkCmdDrawIndexed(currentCommandBuffer, static_cast<uint32_t>(curmesh.indices.size()), 1, 0, 0, 0);
